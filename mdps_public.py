093c36b431adab18ee2d1d396a0787c3363bade1
HariniElamurugan <harini6173@gmail.com>
2025-07-30 13:22:59 +0530

    Update mdps_public.py

--- mdps_public.py
+++ mdps_public.py
@@ -12,11 +12,11 @@ from streamlit_option_menu import option_menu
 
 # loading the saved models
 
-diabetes_model = pickle.load(open('C:\Users\sanjay\Downloads\scaler_diabetes.sav', 'rb'))
+diabetes_model = pickle.load(open(r'C:\Users\sanjay\Downloads\scaler_diabetes.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('C:\Users\sanjay\Downloads\scaler_heart.sav', 'rb'))
+heart_disease_model = pickle.load(open(r'C:\Users\sanjay\Downloads\scaler_heart.sav', 'rb'))
 
-parkinsons_model = pickle.load(open('C:\Users\sanjay\Downloads\scaler_parkinsons.sav', 'rb'))
+parkinsons_model = pickle.load(open(r'C:\Users\sanjay\Downloads\scaler_parkinsons.sav', 'rb'))
 
 
 
